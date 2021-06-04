a6fc4123917989ddc5d2e8645d9d23ef233ba3c7
Eleni <elesxn68@gmail.com>
2021-06-04 22:11:24 +0300

    - Removed all recent search bar code (didnt work anyway). - Made some slight code changes and better organised the files. System back to its working state.

--- GUI/js/app.js
+++ GUI/js/app.js
@@ -1,3 +1,6 @@
+/*This whole file is in working process */
+
+/*Functions used for search bar functionality*/
 function search_everything() {
     let input = document.getElementById('home_and_charts_query').value
     input=input.toLowerCase();
@@ -13,17 +16,23 @@ function search_everything() {
     }
 }
 
+function createListItem(newItem) {
+    let li = document.createElement('li');
+    li.textContent = newItem;
+    return li;
+}
+
 function search_news() {
-    let input = document.getElementById('home_and_charts_query').value
-    input=input.toLowerCase();
-    let x = ["AMC"];
-      
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="list-item";                 
+    let stringToSearch = document.getElementById('news_query').value;
+    stringToSearch = stringToSearch.toLowerCase();
+    const availableNews =  document.getElementsByClassName('news_title');
+    
+    var news_found_list = document.getElementById("news_found_list");
+    var listItem = document.getElementById("li");
+
+    for (i = 0; i < availableNews.length; i++) { 
+        if (availableNews[i].innerHTML.toLowerCase().includes(stringToSearch)) {
+            news_found_list.appendChild(createListItem(availableNews[i].innerHTML));
         }
     }
 }
