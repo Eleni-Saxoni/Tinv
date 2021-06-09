2d5a2cd1e86b4fdb2d4b0e20078b4e9a8248ab75
Eleni <elesxn68@gmail.com>
2021-06-10 00:15:01 +0300

    Complete charts.html! Horray!

--- GUI/js/app.js
+++ GUI/js/app.js
@@ -1,37 +1,10 @@
-//createListItem, search: used for search bar functionality
-function createListItem(newItem) {
-    let li = document.createElement('li');
-    li.textContent = newItem;
-    return li;
-}
-
-function search(searchCode) {
-    if (searchCode=="news") {
-        let stringToSearch = document.getElementById('news_query').value;
-        stringToSearch = stringToSearch.toLowerCase();
-        const availableNews =  document.getElementsByClassName('news_title');
-
-        var news_found_list = document.getElementById("news_found_list");
-        var listItem = document.getElementById("li");
-    
-        for (i = 0; i < availableNews.length; i++) { 
-            if (availableNews[i].innerHTML.toLowerCase().includes(stringToSearch)) {
-                news_found_list.appendChild(createListItem(availableNews[i].innerHTML));
-            }
-        }
-    }
-    if (searchCode=="symbols") {
-
-
-    }  
-}
 
 //Creates the plot of existing stock data in watchlist.html
-function depictTimeSeries() {
+function createTimeSeries(symbol, plotId) {
 
     //Creates the actual plots
     function makeplot() {
-        Plotly.d3.csv("files/AMC.csv", function(data){ processData(data) } );
+        Plotly.d3.csv("files/"+symbol+".csv", function(data){ processData(data) } );
     };
         
     //Loads the data from the csv
@@ -43,43 +16,10 @@ function depictTimeSeries() {
             x.push( row['Hour'] );
             y.push( row['Close'] );
         }
-        makePlotly( x, y );
-    }
-
-    //Creates the axes using the data loaded
-    function makePlotly( x, y ){
-        var traces = [{
-            x: x, 
-            y: y
-        }];
-        var layout = {
-            xaxis: {dtick: 30},
-        }
-        Plotly.newPlot('stock_plot', traces, layout);
-    };
-    makeplot();
-}
-
-
-//Creates the plot of a stock, including its prediction, in charts.html
-function predictTimeSeries(symbol, time) {
-    console.log("You just called me with arguments", symbol, time); //--------------------------------------------TO GO!!!
-
-    function makeplot() {
-        Plotly.d3.csv("files/"+symbol+".csv", function(data){ processData(data, time) } );
-    };
-        
-    function processData(allRows, timeToPredict) {
-        var x = [], y = [];
-
-        for (var i=0; i<allRows.length; i++) {
-            row = allRows[i];
-            x.push( row['Hour'] );
-            y.push( row['Close'] );
-        }
         makePlotly( x, y);
     }
 
+    //Creates the axes using the data loaded
     function makePlotly(x, y){
         var traces = [{
             x: x, 
@@ -89,6 +29,56 @@ function predictTimeSeries(symbol, time) {
             xaxis: {dtick: 30},
             title: symbol
         }
+        Plotly.newPlot(plotId, traces, layout);
+    };
+    makeplot();
+}
+
+
+//Creates the plot of a stock, including its prediction, in charts.html
+function predictTimeSeries(symbol) {
+    function makeplot() {
+        Plotly.d3.csv("files/"+symbol+".csv", function(data){ processOldData(data) } );
+    };
+        
+    function processOldData(allRows) {
+        var x = [], y = [];
+
+        for (var i=0; i<allRows.length; i++) {
+            row = allRows[i];
+            x.push( row['Hour'] );
+            y.push( row['Close'] );
+        }
+        Plotly.d3.csv("files/"+symbol+"prediction.csv", function(data){ processAllData(data, x, y) } );
+    }
+
+    function processAllData(allRows, x1, y1) {
+        var x2 = [], y2 = [];
+
+        for (var i=0; i<allRows.length; i++) {
+            row = allRows[i];
+            x2.push( row['Hour'] );
+            y2.push( row['Close'] );
+        }
+        makePlotly(x1, y1, x2, y2);
+    }
+
+    function makePlotly(x1, y1, x2, y2){
+        var trace1 = {
+            x: x1, 
+            y: y1,
+            name:"Existing prices"
+        };
+        var trace2 = {
+            x: x2, 
+            y: y2,
+            name:"Prediction"
+        };
+        var layout = {
+            title: symbol,
+            xaxis: {dtick: 30},
+        };
+        var traces = [trace1, trace2];
         Plotly.newPlot('plot', traces, layout);
     };
     makeplot();
